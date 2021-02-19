699a24b60028a6d694f754218a858d7269f6490f
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2021-02-19 11:45:00 +0100

    Initial commit

---
+++ CustomBgm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using GlobalEnums;
+using Modding;
+using UnityEngine;
+
+namespace CustomBgm
+{
+    class CustomBgm : Mod
+    {
+        internal static CustomBgm Instance;
+
+        private readonly string FOLDER = "CustomBgm";
+        private readonly string DIR;
+
+        public override string GetVersion()
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string ver = asm.GetName().Version.ToString();
+            SHA1 sha1 = SHA1.Create();
+            FileStream stream = File.OpenRead(asm.Location);
+            byte[] hashBytes = sha1.ComputeHash(stream);
+            string hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+            stream.Close();
+            sha1.Clear();
+            return $"{ver}-{hash.Substring(0, 6)}";
+        }
+
+        public CustomBgm() : base("Custom Background Music")
+        {
+            Instance = this;
+
+            switch (SystemInfo.operatingSystemFamily)
+            {
+                case OperatingSystemFamily.MacOSX:
+                    DIR = Path.GetFullPath(Application.dataPath + "/Resources/Data/Managed/Mods/" + FOLDER);
+                    break;
+                default:
+                    DIR = Path.GetFullPath(Application.dataPath + "/Managed/Mods/" + FOLDER);
+                    break;
+            }
+            if (!Directory.Exists(DIR))
+            {
+                Directory.CreateDirectory(DIR);
+            }
+
+            InitCallbacks();
+        }
+
+        public override void Initialize()
+        {
+            Log("Initializing");
+            Instance = this;
+
+            Log("Initialized");
+        }
+
+        private void InitCallbacks()
+        {
+            // Hooks
+            On.AudioManager.ApplyMusicCue += OnAudioManagerApplyMusicCue;
+        }
+
+        private void OnAudioManagerApplyMusicCue(On.AudioManager.orig_ApplyMusicCue orig, AudioManager self, MusicCue musicCue, float delayTime, float transitionTime, bool applySnapshot)
+        {
+            bool changed = false;
+            var infosFieldInfo = musicCue.GetType().GetField("channelInfos", BindingFlags.NonPublic | BindingFlags.Instance);
+            MusicCue.MusicChannelInfo[] infos = (MusicCue.MusicChannelInfo[]) infosFieldInfo.GetValue(musicCue);
+
+            foreach (MusicCue.MusicChannelInfo info in infos)
+            {
+                var audioFieldInfo = info.GetType().GetField("clip", BindingFlags.NonPublic | BindingFlags.Instance);
+                AudioClip origAudio = (AudioClip) audioFieldInfo.GetValue(info);
+
+                if (origAudio != null)
+                {
+                    AudioClip possibleReplace = GetAudioClip(origAudio.name);
+                    if (possibleReplace != null)
+                    {
+                        // Change Audio Clip
+                        audioFieldInfo.SetValue(info, possibleReplace);
+                        changed = true;
+                    }
+                }
+            }
+            if (changed)
+            {
+                infosFieldInfo.SetValue(musicCue, infos);
+            }
+
+            orig(self, musicCue, delayTime, transitionTime, applySnapshot);
+        }
+
+        private AudioClip GetAudioClip(string origName)
+        {
+            if (File.Exists($"{DIR}/{origName}.wav"))
+            {
+                Log($"Using audio file \"{origName}.wav\"");
+                return WavUtility.ToAudioClip($"{DIR}/{origName}.wav");
+            }
+            Log($"Using original for \"{origName}\"");
+            return null;
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# CusotmBgm
+
+This mod enables the use of custom background music in a similar way old versions of CustomKnight did.
+
+## Adding custom music
+
+1. Install the mod
+2. Have `.wav` audio files
+3. Run the game once and go to the place you want the custom audio in
+    1. This generates a folder
+    2. This also shows the name of the audio, visible in `Modlog.txt`
+4. Exit the game
+5. Go to the `CustomBgm` folder
+    1. This is located next to the `CustomKnight` folder and also next to mod files
+6. Move the audio files here and rename them according to step 3.2.
+7. Start the game
+8. Go to the place you wanted the audio in, if everything worked correctly, it should be a bit laggy for a short moment, but the audio should play
